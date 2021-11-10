52efe2b152fd3a23694ad8f094857f3ba42b0e85
Tengo <tengosmail@gmail.com>
2021-11-10 23:22:08 +0400

    Adding Categoeries form

--- admin/categories.php
+++ admin/categories.php
@@ -1,4 +1,5 @@
 <?php
+
 include "includes/header.php";
 ?>
 
@@ -19,17 +20,64 @@ include "includes/header.php";
                         <small></small>
                     </h1>
                     <div class="col-xs-6">
-                        <form action="" method="post">
+
+                        <?php
+                        if(isset($_POST['submit'])){
+                            if ($_POST['cat_title'] == "" || empty($_POST['cat_title'])){
+                                echo "<h3 style='color: red'>Form Must Be filled</h3>";
+                            }else{
+
+                                $query = "INSERT INTO categories( cat_title) VALUE('{$_POST['cat_title']}')";
+                                $res = mysqli_query($connection,$query);
+                                if ($res){
+                                    echo "Category Succesfuly Added";
+                                }else{
+                                    echo "Cannot Add category".mysqli_error($connection);
+                                }
+                             }
+                        }
+
+
+
+
+                        ?>
+                        <form action="#" method="post">
 
                             <div class="form-group">
                                 <label for="cat_title">Add Category</label>
                                 <input class="form-control" type="text" name="cat_title" id="">
                             </div>
                             <div class="form-group">
-                                <input class="btn btn-primary" type="button" name="submit" value="Submit">
+                                <input class="btn btn-primary" type="submit" name="submit" value="Submit">
                             </div>
 
                         </form>
+                    </div>
+                    <div class="col-xs-6">
+                        <table class="table table-hover table-bordered">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                            </tr>
+                            <?php
+                            $query = "SELECT * FROM categories";
+                            $select_all_cat = mysqli_query($connection,$query);
+                            ?>
+                            </thead>
+                            <tbody>
+
+                                   <?php
+                                   while ($row = mysqli_fetch_assoc($select_all_cat)){
+                                       echo " <tr>";
+                                       echo "<td> ".$row['cat_id']. "</td><td> ".$row['cat_title']. "</td>";
+                                       echo " </tr>";
+                                   }
+                                   ?>
+
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
 
 
--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -148,7 +148,7 @@
 <!--                <a href="forms.html"><i class="fa fa-fw fa-edit"></i> Forms</a>-->
 <!--            </li>-->
             <li>
-                <a href="bootstrap-elements.html"><i class="fa fa-fw fa-desktop"></i> Bootstrap Elements</a>
+                <a href="categories.php"><i class="fa fa-fw fa-desktop"></i> Categories</a>
             </li>
             <li>
                 <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Bootstrap Grid</a>
